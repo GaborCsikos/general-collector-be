1b6492859d7bcfb5a23467b38a4c4a7b64e40321
gcsikos <csiki123@gmail.com>
2022-06-12 22:07:39 +0200

    Initial commit

---
+++ src/main/java/hu/gcsikos/collector/collectorbackendms/CollectorBackendMsApplication.java
@@ -0,0 +1,13 @@
+package hu.gcsikos.collector.collectorbackendms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorBackendMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectorBackendMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/gcsikos/collector/collectorbackendms/CollectorBackendMsApplicationTests.java
@@ -0,0 +1,15 @@
+package hu.gcsikos.collector.collectorbackendms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CollectorBackendMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
